c191fe3968a7a7e406ae2e6c225ab8af9d3b1be4
Sébastien Touzet <calibx@gmail.com>
2021-12-27 22:03:51 +0100

    ligth

---
+++ MsfsPlugin/MsfsPlugin/folder/LightsDynamicFolder.cs
@@ -0,0 +1,38 @@
+﻿namespace Loupedeck.MsfsPlugin.folder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LightsDynamicFolder : PluginDynamicFolder
+    {
+        public LightsDynamicFolder()
+        {
+            this.DisplayName = "Lights";
+            this.GroupName = "System";
+            this.Navigation = PluginDynamicFolderNavigation.EncoderArea;
+
+        }
+        public override IEnumerable<String> GetButtonPressActionNames()
+        {
+            return new[]
+            {
+        PluginDynamicFolder.NavigateUpActionName,
+        this.CreateCommandName("7"),
+        this.CreateCommandName("8"),
+        this.CreateCommandName("9"),
+        this.CreateCommandName("."),
+        this.CreateCommandName("4"),
+        this.CreateCommandName("5"),
+        this.CreateCommandName("6"),
+        this.CreateCommandName("0"),
+        this.CreateCommandName("1"),
+        this.CreateCommandName("2"),
+        this.CreateCommandName("3")
+    };
+        }
+    }
+
+}
