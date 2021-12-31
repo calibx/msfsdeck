55c12dd2fa7c17012ca947feed7794082ae542f7
NZXT\calib <calibx@gmail.com>
2021-12-31 17:48:06 +0100

    Correct pitot input name

--- MsfsPlugin/MsfsPlugin/input/misc/PitotInput.cs
+++ MsfsPlugin/MsfsPlugin/input/misc/PitotInput.cs
@@ -7,7 +7,7 @@
     class PitotInput : DefaultInput
     {
         public PitotInput() : base("Pitot", "Pitot heating", "Misc") { }
-        protected override String GetValue() => MsfsData.Instance.CurrentPitot ? "Disable Pitot" : "Enable Pitot";
+        protected override String GetValue() => MsfsData.Instance.CurrentPitot ? "Disable Pitot Heating" : "Enable Pitot Heating";
         protected override void ChangeValue() => MsfsData.Instance.CurrentPitot = !MsfsData.Instance.CurrentPitot;
     }
 }
