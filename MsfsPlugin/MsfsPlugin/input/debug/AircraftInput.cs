97bb80eba25ee130b8c0eb08a226764504abb71e
NZXT\calib <calibx@gmail.com>
2022-01-10 21:27:44 +0100

    Fix DA40 Cabin light

---
+++ MsfsPlugin/MsfsPlugin/input/debug/AircraftInput.cs
@@ -0,0 +1,13 @@
+﻿namespace Loupedeck.MsfsPlugin
+{
+    using System;
+
+    using Loupedeck.MsfsPlugin.input;
+
+    class AircraftInput : DefaultInput
+    {
+        public AircraftInput() : base("Aircraft", "Display current aircraft name", "Debug") { }
+        protected override String GetValue() => MsfsData.Instance.AircraftName;
+    }
+}
+
