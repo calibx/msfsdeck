91f3770a8832048375bbaa29edf58afe9ed33c52
NZXT\calib <calibx@gmail.com>
2022-02-20 12:15:03 +0100

    Rename auto to Arm

--- MsfsPlugin/MsfsPlugin/encoder/SpoilerEncoder.cs
+++ MsfsPlugin/MsfsPlugin/encoder/SpoilerEncoder.cs
@@ -6,7 +6,7 @@
     class SpoilerEncoder : DefaultEncoder
     {
         public SpoilerEncoder() : base("Spoiler", "Spoiler position", "Nav", true, -1, 10, 1) { }
-        protected override String GetDisplayValue() => this.GetValue() == -1 ? "Auto" : this.GetValue().ToString();
+        protected override String GetDisplayValue() => this.GetValue() == -1 ? "Arm" : this.GetValue().ToString();
         protected override void RunCommand(String actionParameter) => this.SetValue(0);
         protected override Int32 GetValue() => MsfsData.Instance.CurrentSpoiler;
         protected override void SetValue(Int32 newValue) => MsfsData.Instance.CurrentSpoiler = newValue;
