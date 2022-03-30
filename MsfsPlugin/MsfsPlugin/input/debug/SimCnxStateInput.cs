4a5b2d7b6e05faaf73aaa53d5f3a96a1cff3945c
NZXT\calib <calibx@gmail.com>
2022-03-30 21:56:29 +0200

    Add brakes and gear

---
+++ MsfsPlugin/MsfsPlugin/input/debug/SimCnxStateInput.cs
@@ -0,0 +1,22 @@
+﻿namespace Loupedeck.MsfsPlugin
+{
+    using System;
+
+    using Loupedeck.MsfsPlugin.input;
+    using Loupedeck.MsfsPlugin.msfs;
+    class SimCnxStateInput : DefaultInput
+    {
+        private Boolean heartbeat;
+        public SimCnxStateInput() : base("ConnectionSimConnect", "Display SimConnect connection state", "Debug") { }
+
+        protected override String GetValue()
+        {
+            this.heartbeat = !this.heartbeat;
+            return (MsfsData.Instance.SimConnected ? "Connected" : MsfsData.Instance.SimTryConnect ? "Trying to connect" : "Disconnected") + (this.heartbeat ? "\n..." : "\n. .");
+        }
+
+        protected override void ChangeValue() => SimConnectDAO.Instance.Connect();
+
+    }
+}
+
