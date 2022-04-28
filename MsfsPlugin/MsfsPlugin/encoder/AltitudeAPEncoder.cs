0851a9955b3d94c5a3e31a48e9f04625b694170e
NZXT\calib <calibx@gmail.com>
2022-04-28 10:07:30 +0200

    Fix clic

--- MsfsPlugin/MsfsPlugin/encoder/AltitudeAPEncoder.cs
+++ MsfsPlugin/MsfsPlugin/encoder/AltitudeAPEncoder.cs
@@ -14,7 +14,7 @@
             this._bindings.Add(bind);
             MsfsData.Instance.Register(bind);
         }
-        protected override void RunCommand(String actionParameter) => this.SetValue(this._bindings[0].ControllerValue);
+        protected override void RunCommand(String actionParameter) => this.SetValue(this._bindings[1].ControllerValue);
         protected override String GetDisplayValue() => "[" + this._bindings[0].ControllerValue + "]\n" + this._bindings[1].ControllerValue;
         protected override Int64 GetValue() => this._bindings[0].ControllerValue;
         protected override void SetValue(Int64 newValue) => this._bindings[0].SetControllerValue(newValue);
