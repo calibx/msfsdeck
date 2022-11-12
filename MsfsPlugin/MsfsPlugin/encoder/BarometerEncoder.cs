dd6d16bda527c7f4c1cb560e5012e99a96007753
calibx <calibx@gmail.com>
2022-11-12 17:34:01 +0100

    Add push baro => 29.92

--- MsfsPlugin/MsfsPlugin/encoder/BarometerEncoder.cs
+++ MsfsPlugin/MsfsPlugin/encoder/BarometerEncoder.cs
@@ -7,7 +7,7 @@
     class BarometerEncoder : DefaultEncoder
     {
         public BarometerEncoder() : base("Baro", "Barometer encoder", "Nav", true, 2799, 3201, 1) => this._bindings.Add(MsfsData.Instance.Register(new Binding(BindingKeys.KOHLSMAN)));
-        protected override void RunCommand(String actionParameter) => this.SetValue(this._bindings[0].ControllerValue);
+        protected override void RunCommand(String actionParameter) => this.SetValue(2992);
         protected override String GetDisplayValue() => (this._bindings[0].ControllerValue / 100f).ToString();
         protected override Int64 GetValue() => this._bindings[0].ControllerValue;
         protected override void SetValue(Int64 newValue) => this._bindings[0].SetControllerValue(newValue);
