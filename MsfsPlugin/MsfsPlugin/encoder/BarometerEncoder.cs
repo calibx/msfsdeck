6e1292bf57c35089d8ac6773ab8a8e766390cfbc
Jesper Jørgensen <jesper.joergensen@youmail.dk>
2023-12-02 18:38:22 +0100

    Cleanup/refactorization

--- MsfsPlugin/MsfsPlugin/encoder/BarometerEncoder.cs
+++ MsfsPlugin/MsfsPlugin/encoder/BarometerEncoder.cs
@@ -1,17 +1,18 @@
 ﻿namespace Loupedeck.MsfsPlugin
 {
-    using System;
-
     using Loupedeck.MsfsPlugin.encoder;
 
     class BarometerEncoder : DefaultEncoder
     {
-        public BarometerEncoder() : base("Baro", "Barometer encoder", "Nav", true, 2799, 3201, 1) => this._bindings.Add(MsfsData.Instance.Register(new Binding(BindingKeys.KOHLSMAN)));
-        protected override void RunCommand(String actionParameter) => this.SetValue(2992);
-        protected override String GetDisplayValue() => (this._bindings[0].ControllerValue / 100f).ToString();
-        protected override Int64 GetValue() => this._bindings[0].ControllerValue;
-        protected override void SetValue(Int64 newValue) => this._bindings[0].SetControllerValue(newValue);
+        public BarometerEncoder() : base("Baro", "Barometer encoder", "Nav", true, 2799, 3201, 1) =>
+            _bindings.Add(MsfsData.Instance.Register(BindingKeys.KOHLSMAN));
 
+        protected override void RunCommand(string actionParameter) => SetValue(2992);
 
+        protected override string GetDisplayValue() => (_bindings[0].ControllerValue / 100f).ToString();
+
+        protected override long GetValue() => _bindings[0].ControllerValue;
+
+        protected override void SetValue(long newValue) => _bindings[0].SetControllerValue(newValue);
     }
 }
