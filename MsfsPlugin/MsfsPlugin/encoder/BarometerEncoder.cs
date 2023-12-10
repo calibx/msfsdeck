7126dbcacc5f2b8773ad4059fd90ff2faf65b4bc
Jesper Jørgensen <jesper.joergensen@youmail.dk>
2023-12-10 10:13:08 +0100

    Barometer encoder: command is now setting altimeter to sea level pressure
    Previously, it set the altimeter to the standard pressure.

--- MsfsPlugin/MsfsPlugin/encoder/BarometerEncoder.cs
+++ MsfsPlugin/MsfsPlugin/encoder/BarometerEncoder.cs
@@ -6,10 +6,13 @@
 
     class BarometerEncoder : DefaultEncoder
     {
-        public BarometerEncoder() : base("Baro", "Barometer encoder", "Nav", true, 2799, 3201, 1) =>
+        public BarometerEncoder() : base("Baro", "Barometer encoder", "Nav", true, 2799, 3201, 1)
+        {
             bindings.Add(MsfsData.Instance.Register(BindingKeys.KOHLSMAN));
+            bindings.Add(MsfsData.Instance.Register(BindingKeys.SEA_LEVEL_PRESSURE));
+        }
 
-        protected override void RunCommand(string actionParameter) => SetValue(2992);
+        protected override void RunCommand(string actionParameter) => SetValue((long)Math.Round(bindings[1].MsfsValue / inHg2mbar));
 
         protected override string GetDisplayValue() => (GetValue() / 100f).ToString() + "\n" + GetValueHpa().ToString();
 
@@ -19,8 +22,10 @@
 
         private double GetValueHpa()
         {
-            var result = bindings[0].ControllerValue * 0.3386389;
+            var result = bindings[0].ControllerValue * inHg2mbar;
             return Math.Round(result, 1);
         }
+
+        private const double inHg2mbar = 0.3386389;
     }
 }
