dd08e571dab5ea33c1706c8753b8fd91f2bc936f
Jesper Jørgensen <jesper.joergensen@youmail.dk>
2023-12-10 10:11:12 +0100

    Connection input can now connect/disconnect

--- MsfsPlugin/MsfsPlugin/input/debug/SimCnxStateInput.cs
+++ MsfsPlugin/MsfsPlugin/input/debug/SimCnxStateInput.cs
@@ -1,10 +1,22 @@
 ﻿namespace Loupedeck.MsfsPlugin
 {
     using Loupedeck.MsfsPlugin.input;
+    using Loupedeck.MsfsPlugin.msfs;
+
     class SimCnxStateInput : DefaultInput
     {
         public SimCnxStateInput() : base("ConnectionSimConnect", "Display SimConnect connection state", "Debug") => bindings.Add(MsfsData.Instance.Register(BindingKeys.CONNECTION));
+
         protected override string GetValue() => bindings[0].MsfsValue == 1 ? "Connected" : bindings[0].MsfsValue == 2 ? "Trying to connect" : "Disconnected";
+
+        protected override void RunCommand(string actionParameter)
+        {
+            var curValue = bindings[0].MsfsValue;
+            if ( curValue == 1 || curValue == 2)
+                SimConnectDAO.Instance.Disconnect();
+            else
+                SimConnectDAO.Instance.Connect();
+        }
     }
 }
 
