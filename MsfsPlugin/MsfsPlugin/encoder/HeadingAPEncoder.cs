251fb371300a9b49380ffd7670ae12def39ce5bd
Jesper Jørgensen <jesper.joergensen@youmail.dk>
2024-01-21 14:48:55 +0100

    Corrected name of AP-heading encoder
    NOTE!: After getting this change, you need to reassign any encoder bound assigned to this function.

---
+++ MsfsPlugin/MsfsPlugin/encoder/HeadingAPEncoder.cs
@@ -0,0 +1,27 @@
+﻿namespace Loupedeck.MsfsPlugin
+{
+    using Loupedeck.MsfsPlugin.encoder;
+    using Loupedeck.MsfsPlugin.tools;
+
+    class HeadingAPEncoder : DefaultEncoder
+    {
+        public HeadingAPEncoder() : base("Head", "Autopilot heading encoder", "AP", true, 1, 360, 1)
+        {
+            bindings.Add(ApHeading = Register(BindingKeys.AP_HEADING));
+            bindings.Add(Heading = Register(BindingKeys.HEADING));
+        }
+
+        protected override void RunCommand(string actionParameter) => SetValue(Heading.ControllerValue);
+        protected override string GetDisplayValue() => "[" + ApHeading.ControllerValue + "]\n" + Heading.ControllerValue;
+        protected override long GetValue() => ApHeading.ControllerValue;
+        protected override void SetValue(long newValue) => ApHeading.SetControllerValue(newValue);
+        protected override void ApplyAdjustment(string actionParameter, int ticks)       //>> Kan undværes hvis DefaultEncoder kender til cycle /default-værdi false)
+        {
+            SetValue(ConvertTool.ApplyAdjustment(GetValue(), ticks, min, max, step, true));
+            ActionImageChanged();
+        }
+
+        readonly Binding ApHeading;
+        readonly Binding Heading;
+    }
+}
